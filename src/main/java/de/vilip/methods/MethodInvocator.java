e5139fd5a09a437b227e5407622d54051433f5ae
vvilip <phillipsf@tutanota.com>
2025-03-17 15:49:05 +0100

    ✨ Add Method invocator

---
+++ src/main/java/de/vilip/methods/MethodInvocator.java
@@ -0,0 +1,33 @@
+package de.vilip.methods;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class MethodInvocator
+{
+	public void runTestMethods(List<Method> methods)
+	{
+		for (Method m : methods)
+		{
+			runMethod(m);
+		}
+	}
+
+	private void runMethod(Method method)
+	{
+		Class<?> clazz = method.getDeclaringClass();
+		try
+		{
+			method.invoke(clazz.getDeclaredConstructor().newInstance());
+		}
+		catch (NoSuchMethodException | InstantiationException | IllegalAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+		catch (InvocationTargetException e)
+		{
+
+		}
+	}
+}
