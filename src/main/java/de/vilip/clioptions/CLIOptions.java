8876c48bd4a980baf27e9f965ed100bc5a9e28c8
vvilip <phillipsf@tutanota.com>
2025-04-09 12:37:45 +0200

    ♻️ Refactores CliDefinitions

--- src/main/java/de/vilip/clioptions/CLIOptions.java
+++ src/main/java/de/vilip/clioptions/CLIOptions.java
@@ -1,18 +1,20 @@
 package de.vilip.clioptions;
 
+import org.apache.commons.cli.Option;
+
 public enum CLIOptions
 {
-	TEST("t"), DIRECTORY("d"), FILE("f");
+	TEST(new Option("t", true, "A whole test directory")), DIRECTORY(new Option("d", true, "A single test file")), FILE(new Option("f", true, "A single test in a file"));
 
-	private final String shortOption;
+	private final Option option;
 
-	CLIOptions(String shortOption)
+	CLIOptions(Option option)
 	{
-		this.shortOption = shortOption;
+		this.option = option;
 	}
 
-	public String getShortOption()
+	public Option getOption()
 	{
-		return shortOption;
+		return option;
 	}
 }
--- src/main/java/de/vilip/clioptions/OptionsDefinition.java
+++ src/main/java/de/vilip/clioptions/OptionsDefinition.java
@@ -1,19 +1,13 @@
 package de.vilip.clioptions;
 
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
 public class OptionsDefinition
 {
 	public static Options initalizeOptions()
 	{
-		Option directory = new Option(CLIOptions.DIRECTORY.getShortOption(), true, "A whole test directory");
-		Option file = new Option(CLIOptions.FILE.getShortOption(), true, "A single test file");
-		Option test = new Option(CLIOptions.TEST.getShortOption(), true, "A single test in a file");
-		Options options = new Options();
-
-		return options.addOption(directory)
-			.addOption(file)
-			.addOption(test);
+		return new Options().addOption(CLIOptions.DIRECTORY.getOption())
+			.addOption(CLIOptions.FILE.getOption())
+			.addOption(CLIOptions.TEST.getOption());
 	}
 }
